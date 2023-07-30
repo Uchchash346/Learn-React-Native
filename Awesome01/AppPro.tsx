e12c691536089471e2e6bfc36dfed3d233a18b75
Uchchash346 <mru416@gmail.com>
2023-07-30 06:36:51 +0600

    How to create react native project, some basic texts and styles

---
+++ Awesome01/AppPro.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+
+import { Text, View, StyleSheet, useColorScheme } from 'react-native';
+
+function AppPro(): JSX.Element {
+    const isDarkMode = useColorScheme() === 'dark';
+    return (
+        <View style={styles.container}>
+            <Text style={isDarkMode ? styles.whiteText : styles.darkText}>Hello Uchchash</Text>
+        </View >
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    whiteText: {
+        color: '#FFFFFF',
+    },
+    darkText: {
+        color: '#000000',
+    }
+})
+
+export default AppPro;
